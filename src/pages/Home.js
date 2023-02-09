0e5961642264a7bd44f2fcbb5d1fe4d858901dcf
arun2604 <54104684+arun2604@users.noreply.github.com>
2023-02-09 17:06:55 +0530

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import CollapseTable from '../components/Table'
+import Upload from '../components/Upload'
+
+function Home() {
+  const [ excelData,setExcelData ] = useState(null)
+
+  return (
+    <div>
+      <Upload setExcelData={setExcelData} />
+      <CollapseTable excelData={excelData}  setExcelData={setExcelData}/> 
+    </div>
+  )
+}
+
+export default Home
